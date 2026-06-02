5e5570b79cc8c91774a07b09a0afb14c768882a8
Yosuke Sekiya <zanezanekichi113@gmail.com>
2026-06-03 00:56:07 +0900

    iPhone画面表示安定化_v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next"; // ⭐️ Viewport を追加
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
